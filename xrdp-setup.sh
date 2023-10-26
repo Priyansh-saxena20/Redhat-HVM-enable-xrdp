60f85b6804975598ed69e34469488da224f0e532
Priyansh saxena <priyansh.saxena20@gmail.com>
2023-10-27 03:16:59 +0530

    Update  firewall cmd  permanent  in xrdp-setup.sh

--- xrdp-setup.sh
+++ xrdp-setup.sh
@@ -5,3 +5,5 @@ yum install tigervnc-server -y
 wget https://dl.fedoraproject.org/pub/epel/epel-release-latest-9.noarch.rpm
 yum install xrdp -y
 systemctl start xrdp.service
+firewall-cmd --permanent --add-port=3389/tcp
+firewall-cmd --reload
